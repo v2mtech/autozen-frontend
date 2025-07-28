914e166362206be8782dc5bd32d08380d1a534bc
v2mtech <mgraciolli6@gmail.com>
2025-07-28 19:30:57 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosHeaders } from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://ceb1f3f031ab.ngrok-free.app',
+  baseURL: 'https://7fa932ada88f.ngrok-free.app', // <-- O LINK DA SUA IMAGEM
 });
 
 // Interceptor para adicionar o token JWT em todas as requisições
@@ -9,9 +9,7 @@ api.interceptors.request.use((config) => {
   const token = localStorage.getItem('@nanisound:token');
 
   if (token) {
-    // Garante que o cabeçalho de autorização seja definido corretamente
     if (!config.headers) {
-      // Cria uma nova instância de AxiosHeaders em vez de um objeto vazio
       config.headers = new AxiosHeaders();
     }
     config.headers.Authorization = `Bearer ${token}`;
